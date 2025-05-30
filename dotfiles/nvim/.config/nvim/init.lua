453104be8f74fbc0fc6986402ce673c17b55d692
Pascal Erb <paschelino@gmail.com>
2025-05-30 15:48:50 +0200

    fix rbenv and ruby support

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.fn.executable("nu") == 1 then
+  vim.o.shell = "/usr/local/bin/nu --login"
+  vim.o.shellcmdflag = "-c"
+end
+
+-- make extra sure rbenv shims are on PATH for all jobs
+vim.env.PATH = vim.env.HOME .. "/.rbenv/shims:" .. vim.env.HOME .. "/.rbenv/bin:" .. vim.env.PATH

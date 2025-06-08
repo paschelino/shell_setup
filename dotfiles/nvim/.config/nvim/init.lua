b5fcd9a0afb5e97989dd55304c8a6db54b45efd1
Pascal Erb <paschelino@gmail.com>
2025-06-09 00:10:19 +0200

    fix nu shell cmd in nvim

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,9 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Can you make the setup here for nu perfect?
 if vim.fn.executable("nu") == 1 then
-  vim.o.shell = "/usr/local/bin/nu --login"
+  vim.o.shell = "nu"
   vim.o.shellcmdflag = "-c"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
 end
 
 -- make extra sure rbenv shims are on PATH for all jobs

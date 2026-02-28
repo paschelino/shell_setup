7c12180bc0011ff61b8b7e5aa240880fa5b53662
Pascal Erb <paschelino@gmail.com>
2026-02-28 08:15:37 +0100

    add auto refresh (best effort for now)

--- dotfiles/nvim/.config/nvim/lua/config/options.lua
+++ dotfiles/nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,16 @@
 
 vim.opt_local.textwidth = 120
 vim.opt_local.colorcolumn = "120"
+
+-- Automatically reload buffers when files change on the filesystem.
+-- `autoread` alone only triggers in limited scenarios, so the autocmd below
+-- calls `checktime` on focus/buffer events to actively detect changes.
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI", "WinEnter" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})

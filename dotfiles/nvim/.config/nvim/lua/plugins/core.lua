96ca4cb4edaf209a74ebfd8bb7fb783a09762767
Pascal Erb <paschelino@gmail.com>
2026-04-12 18:48:43 +0200

    fix catppuccin config in nvim

--- dotfiles/nvim/.config/nvim/lua/plugins/core.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/core.lua
@@ -2,13 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
-    "catppuccin/nvim",
-    opts = {
-      flavour = "mocha",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }

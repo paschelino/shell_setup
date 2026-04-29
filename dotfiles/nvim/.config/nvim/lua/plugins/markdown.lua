7cee09318b9336b95db8e057fa32144289622ca8
Pascal Erb <paschelino@gmail.com>
2026-04-29 15:26:11 +0200

    chore(lazyvim): reveal html comments in markdown

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      html = {
+        comment = {
+          conceal = false,
+        },
+      },
+    },
+  },
+}

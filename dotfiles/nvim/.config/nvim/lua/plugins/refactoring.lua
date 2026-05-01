d94d99adb8e8b0f120127f1c41dc0e5e3419ea2c
Pascal Erb <paschelino@gmail.com>
2026-05-01 11:46:47 +0200

    chore(neovim): add comment

--- dotfiles/nvim/.config/nvim/lua/plugins/refactoring.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -1,3 +1,5 @@
+-- refactoring.nvim has an undeclared dependency on lewis6991/async.nvim
+-- which LazyVim's extra does not include. Declare it here explicitly.
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = {
